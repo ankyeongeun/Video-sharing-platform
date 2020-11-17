214c5535265c133145e7425c451549dc1ed80746
angyeongeun <anruddms8124@gmail.com>
2020-11-17 11:59:44 +0900

    styling.2

--- middlewares.js
+++ middlewares.js
@@ -11,7 +11,7 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "YouTube-clone";
     res.locals.routes = routes;
     res.locals.user = {
-        isAuthenticated: false,
+        isAuthenticated: true,
         id: 1
     };
 
